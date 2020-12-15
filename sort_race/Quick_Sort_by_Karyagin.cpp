b7fdca9e003a72616ee9223e6e86ace9ad2fda92
OlegKaryagin <63190353+OlegKaryagin@users.noreply.github.com>
2020-12-15 15:48:08 +0300

    Final
    Добавил сортировку для double.

--- sort_race/Quick_Sort_by_Karyagin.cpp
+++ sort_race/Quick_Sort_by_Karyagin.cpp
@@ -42,3 +42,44 @@ vector <int>QuickSort(vector<int> mass)
 	}
 	return sorted;
 }
+
+
+vector <double>QuickSortForDouble(vector<double> mass)
+{
+	vector<double> sorted = mass;
+	vector<int> Stack;
+	Stack.push_back(0);
+	Stack.push_back(sorted.size() - 1);
+	while (!Stack.empty())
+	{
+		int high = Stack.back();
+		Stack.pop_back();
+		int low = Stack.back();
+		Stack.pop_back();
+		int first = low - 1;
+		for (int last = low; last < high; last++)
+		{
+			if (sorted[last] <= sorted[high])
+			{
+				if (first != last)
+				{
+					first++;
+					swap(sorted[first], sorted[last]);
+				}
+			}
+		}
+		swap(sorted[first + 1], sorted[high]);
+		int p = first + 1;
+		if (p - low > 1)
+		{
+			Stack.push_back(low);
+			Stack.push_back(p - 1);
+		}
+		if (high - p > 1)
+		{
+			Stack.push_back(p + 1);
+			Stack.push_back(high);
+		}
+	}
+	return sorted;
+}
